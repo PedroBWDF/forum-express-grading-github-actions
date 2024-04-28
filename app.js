1811731c9c9162b827f22d1e85af825cc7d87d6a
PedroBWDF <bwdf64587@gmail.com>
2024-04-28 23:18:54 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport') // 增加這行，引入 Passport
+const handlebarsHelpers = require('./helpers/handlebars-helpers') // 引入 handlebars-helpers
+const { getUser } = require('./helpers/auth-helpers') // 增加這行，引入自定義的 auth-helpers
 const routes = require('./routes')
 
 const app = express()
@@ -10,8 +12,8 @@ const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret' // 新增這行
 const db = require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
 
-// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs，後再加入handlebarsHelpers 
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true })) // 加入這行
@@ -23,6 +25,7 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req) // 增加這行
   next()
 })
 
