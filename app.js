fcdb206de83bbea55bef107abf2ee775a98c7109
PedroBWDF <bwdf64587@gmail.com>
2024-04-30 16:48:55 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport') // 增加這行，引入 Passport
 const handlebarsHelpers = require('./helpers/handlebars-helpers') // 引入 handlebars-helpers
@@ -12,7 +13,7 @@ const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret' // 新增這行
 const db = require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
 
-// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs，後再加入handlebarsHelpers 
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs，後再加入handlebarsHelpers
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
@@ -22,6 +23,7 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session()) // 增加這行，啟動 session 功能
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method')) // 使用 method-override
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,7 +5,9 @@ const adminController = require('../../controllers/admin-controller')
 // const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
 
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 
