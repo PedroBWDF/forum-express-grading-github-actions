ac224655fcb56788d03115291b0d559d90d12ce1
PedroBWDF <bwdf64587@gmail.com>
2024-05-01 17:11:39 +0800

    feat:add restaurant image

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,13 +4,15 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 // const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
 
+const upload = require('../../middleware/multer') // 載入 multer
+
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant) // 修改後台編輯餐廳的路由，使用multer中間件
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 
 // router.get('/restaurants/create', adminController.createRestaurant)
 // router.get('/restaurants', adminController.getRestaurants)
