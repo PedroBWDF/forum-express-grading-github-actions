3c7b792dd85cca1a8d16d83234060b19ab8b2d97
PedroBWDF <bwdf64587@gmail.com>
2024-05-13 17:36:43 +0800

    add user profile page and edit feature

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
-const db = require('../models')
-const { User } = db
+// const db = require('../models')
+const { User } = require('../models')
+
+const { localFileHandler } = require('../helpers/file-helpers') // 讓程式邏輯可以把image檔案傳到 file-helper 處理
 
 const userController = {
   signUpPage: (req, res) => {
@@ -41,6 +43,50 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/profile', { user: user.toJSON() })
+      })
+  },
+
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/edit', { user: user.toJSON() })
+      })
+  },
+
+  putUser: (req, res, next) => {
+    if (Number(req.params.id) !== Number(req.user.id)) {
+      res.redirect(`/users/${req.params.id}`)
+    }
+
+    const { name } = req.body
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+
+    return Promise.all([
+      User.findByPk(req.params.id),
+      localFileHandler(file) // 把image檔案傳到 file-helper 處理
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
+const upload = require('../middleware/multer')
 
 // 載入admin.js
 const admin = require('./modules/admin')
@@ -26,6 +27,11 @@ router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
 router.get('/restaurants/:id/dashboard', restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants) // 修改這行，新增 authenticated 參數
